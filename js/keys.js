ce65503e19b722cd12c4652063c6904b4be9aae7
Damian Wrather <damian.wrather@gmail.com>
2023-04-30 14:32:21 -0500

    secrets added

---
+++ js/keys.js
@@ -0,0 +1,6 @@
+const keys = {
+    mapbox: process.env.MAPBOX_API_KEY,
+    weathermap: process.env.OPEN_WEATHER_MAP_API_KEY,
+    github_token: process.env.GITHUB_PERSONAL_TOKEN
+}
+export default keys;
